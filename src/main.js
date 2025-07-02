cf15da842714b08da66f9c89692611d6aee69d85
Nipanko nath mazumder <nipanko@protonmail.com>
2025-07-02 19:26:34 +0530

    Initial commit of new portfolio

--- src/main.js
+++ src/main.js
@@ -20,4 +20,29 @@ window.addEventListener("load", () => {
       mainContent.style.display = "block";
     },
   });
+
+
+  // Animate dialog box in
+gsap.from("#dialog-box", {
+  y: -100,
+  opacity: 0,
+  delay: 0.5,
+  duration: 1,
+  ease: "power4.out",
+});
+
+// Close button
+document.querySelector(".btn-close").addEventListener("click", () => {
+  gsap.to("#dialog-box", {
+    y: -100,
+    opacity: 0,
+    duration: 0.5,
+    onComplete: () => {
+      document.getElementById("dialog-box").style.display = "none";
+    },
+  });
+});
+
+
+
 });
